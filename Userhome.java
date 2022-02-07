9f5b46156398c61b35e659479be55ee3e78d45ef
jaya1729 <74919479+jaya1729@users.noreply.github.com>
2022-02-07 11:26:54 +0530

    user screen

---
+++ Userhome.java
@@ -0,0 +1,53 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+
+public class Userhome extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    public Userhome() {
+
+    }
+
+    
+    public Userhome(String userSes) {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(450, 190, 1014, 597);
+        setResizable(false);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        JButton btnNewButton = new JButton("Logout");
+        btnNewButton.setForeground(new Color(0, 0, 0));
+        btnNewButton.setBackground(UIManager.getColor("Button.disabledForeground"));
+        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 39));
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int a = JOptionPane.showConfirmDialog(btnNewButton, "Are you sure?");
+                // JOptionPane.setRootFrame(null);
+                if (a == JOptionPane.YES_OPTION) {
+                    dispose();
+                    Login obj = new Login();
+                    obj.setTitle("Login");
+                    obj.setVisible(true);
+                }
+
+            }
+        });
+        btnNewButton.setBounds(247, 118, 491, 114);
+        contentPane.add(btnNewButton);
+    }
+}
