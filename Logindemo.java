907807457f29058b74a8342937534275fb97b69e
jaya1729 <74919479+jaya1729@users.noreply.github.com>
2022-02-07 11:27:33 +0530

    login demo

---
+++ Logindemo.java
@@ -0,0 +1,23 @@
+package ui;
+
+import java.awt.EventQueue;
+
+public class Logindemo {
+
+	public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Login frame = new Login();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+	}
+
+
