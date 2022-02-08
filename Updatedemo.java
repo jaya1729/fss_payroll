6171760b2c81620f5789568020d8fe9185e69bd2
jaya1729 <74919479+jaya1729@users.noreply.github.com>
2022-02-08 14:58:47 +0530

    update demo

---
+++ Updatedemo.java
@@ -0,0 +1,23 @@
+package ui;
+
+import java.awt.EventQueue;
+
+public class Updatedemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                   
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+	}
+
+}
